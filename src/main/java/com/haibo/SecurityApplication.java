b0a86609e49ea1953f03d6288e822e8fd6a7ac58
HaiboZhu <hbzhu@thoughtworks.com>
2017-02-13 17:12:25 +0800

    [hbzhu] feat: first blood.

---
+++ src/main/java/com/haibo/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.haibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
